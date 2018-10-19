1dfc5790f77014f8cb390bcef5cb478ecf0bfc8a
Sergey Tursanov <tursanov@gmail.com>
2018-10-19 19:43:05 +0400

    В репозиторий добавлен файл include/kkt/xml.h

---
+++ include/kkt/xml.h
@@ -0,0 +1,28 @@
+/* Разбор XML для ККТ. (c) gsr 2018 */
+
+#if !defined KKT_XML_H
+#define KKT_XML_H
+
+#include "sysdefs.h"
+
+#if defined __cplusplus
+extern "C" {
+#endif
+
+/* Типы событий при разборе XML для ККТ */
+#define KKT_XML_BEGIN		0	/* начало XML */
+#define KKT_XML_TAG		1	/* новый тег */
+#define KKT_XML_ETAG		2	/* окончание тега */
+#define KKT_XML_ATTR		3	/* атрибут тега */
+#define KKT_XML_END		4	/* окончание XML */
+
+typedef int (*kkt_xml_callback_t)(bool check, int evt, const char *name, const char *val);
+
+extern bool parse_kkt_xml(char *xml, bool check, kkt_xml_callback_t cbk,
+	char **ep, const char **emsg);
+
+#if defined __cplusplus
+}
+#endif
+
+#endif		/* KKT_XML_H */
